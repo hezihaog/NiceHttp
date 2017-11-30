b3c597e944044c454244e9a41681195afc3e4612
zihe <hezihao@linghit.com>
2017-11-30 21:17:56 +0800

    添加FastJson使用。

--- sample/src/main/java/com/hzh/nice/http/sample/AppContext.java
+++ sample/src/main/java/com/hzh/nice/http/sample/AppContext.java
@@ -2,6 +2,7 @@ package com.hzh.nice.http.sample;
 
 import android.app.Application;
 
+import com.alibaba.fastjson.JSON;
 import com.google.gson.Gson;
 import com.hzh.nice.http.NiceApiClient;
 import com.hzh.nice.http.NiceHttpConfig;
@@ -22,21 +23,30 @@ public class AppContext extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        GsonParser parser = new GsonParser();
+        GsonParser gsonParser = new GsonParser();
+        FastJsonParser fastJsonParser = new FastJsonParser();
         //初始化
         NiceApiClient.init(getApplicationContext(),
-                NiceHttpConfig.newBuild(new ApiByOkHttp(getApplicationContext(), parser),
-                        parser).setDebug(BuildConfig.DEBUG).build());
+                NiceHttpConfig.newBuild(new ApiByOkHttp(getApplicationContext(), gsonParser),
+                        fastJsonParser).setDebug(BuildConfig.DEBUG).build());
     }
 
     /**
-     * Gson转换器
+     * Gson反序列化Json转换器
      */
     private static class GsonParser implements Parser {
 
         @Override
         public <T extends Result> T parse(String json, Class<T> clazz) {
             return new Gson().fromJson(json, clazz);
+        }
+    }
+
+    private static class FastJsonParser implements Parser {
+
+        @Override
+        public <T extends Result> T parse(String json, Class<T> clazz) {
+            return JSON.parseObject(json, clazz);
         }
     }
 }
