c0f7f44393ca22651fcec469168bd44c0844c91d
zihe <hezihao@linghit.com>
2017-11-30 21:31:12 +0800

    添加Jackson使用。

--- sample/src/main/java/com/hzh/nice/http/sample/AppContext.java
+++ sample/src/main/java/com/hzh/nice/http/sample/AppContext.java
@@ -3,12 +3,15 @@ package com.hzh.nice.http.sample;
 import android.app.Application;
 
 import com.alibaba.fastjson.JSON;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.hzh.nice.http.NiceApiClient;
 import com.hzh.nice.http.NiceHttpConfig;
 import com.hzh.nice.http.inter.Parser;
 import com.hzh.nice.http.inter.Result;
 import com.hzh.nice.http.okhttp3.connection.ApiByOkHttp;
+
+import java.io.IOException;
 
 /**
  * @package com.hzh.nice.http.sample
@@ -23,12 +26,18 @@ public class AppContext extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        GsonParser gsonParser = new GsonParser();
-        FastJsonParser fastJsonParser = new FastJsonParser();
+        //gson
+        Parser gsonParser = new GsonParser();
+        //fastJson
+        Parser fastJsonParser = new FastJsonParser();
+        //jackson
+        Parser jacksonParser = new JacksonParser();
+
         //初始化
         NiceApiClient.init(getApplicationContext(),
-                NiceHttpConfig.newBuild(new ApiByOkHttp(getApplicationContext(), gsonParser),
-                        fastJsonParser).setDebug(BuildConfig.DEBUG).build());
+                NiceHttpConfig
+                        .newBuild(new ApiByOkHttp(getApplicationContext(), jacksonParser),
+                                jacksonParser).setDebug(BuildConfig.DEBUG).build());
     }
 
     /**
@@ -42,11 +51,31 @@ public class AppContext extends Application {
         }
     }
 
+    /**
+     * FastJson反序列化Json转换器
+     */
     private static class FastJsonParser implements Parser {
 
         @Override
         public <T extends Result> T parse(String json, Class<T> clazz) {
             return JSON.parseObject(json, clazz);
+        }
+    }
+
+    /**
+     * Jackson反序列化Json转换器
+     */
+    private static class JacksonParser implements Parser {
+
+        @Override
+        public <T extends Result> T parse(String json, Class<T> clazz) {
+            ObjectMapper mapper = new ObjectMapper();
+            try {
+                return mapper.readValue(json, clazz);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
         }
     }
 }
